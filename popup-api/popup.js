de0bc6b340d3c895e49fec7ffc5378b22e2b76a5
Cristiano Fromagio <fromagio_cristiano@live.com>
2018-05-22 08:05:43 -0300

    Alternative approach (consuming api from goqr.me)

---
+++ popup-api/popup.js
@@ -0,0 +1,14 @@
+getCurrentTab().then((tab) => {
+  document.getElementById('image').src = 'https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=' + tab.url;
+});
+
+function getCurrentTab() {
+  return new Promise((resolve, reject) => {
+    browser.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+    }, (tabs) => {
+      resolve(tabs[0]);
+    });
+  }); 
+}
