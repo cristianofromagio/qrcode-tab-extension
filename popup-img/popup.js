b77d610d4a249d0b531053f0339ffa20dfff2c32
Cristiano Fromagio <fromagio_cristiano@live.com>
2018-05-22 08:07:21 -0300

    Alternative approach (base64 encoded data URI from neocotic/qrious)

---
+++ popup-img/popup.js
@@ -0,0 +1,22 @@
+getCurrentTab().then((tab) => {
+  var config = {
+    level: 'H',
+    size: 250,
+    value: tab.url
+  };
+
+  var qr = new QRious(config);
+  var qrImage = qr.toDataURL('image/jpeg');
+  document.getElementById('image').src = qrImage;
+});
+
+function getCurrentTab() {
+  return new Promise((resolve, reject) => {
+    browser.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+    }, (tabs) => {
+      resolve(tabs[0]);
+    });
+  }); 
+}
