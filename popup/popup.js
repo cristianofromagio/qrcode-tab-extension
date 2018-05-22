428a4760bda2d6e10d030392892f23036f330bc2
Cristiano Fromagio <fromagio_cristiano@live.com>
2018-05-22 06:45:56 -0300

    256px QR code

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,8 @@
 getCurrentTab().then((tab) => {
   var config = {
     text: tab.url,
-    width: 150,
-    height: 150,
+    width: 256,
+    height: 256,
     colorDark : "#000000",
     colorLight : "#ffffff",
     correctLevel : QRCode.CorrectLevel.H
