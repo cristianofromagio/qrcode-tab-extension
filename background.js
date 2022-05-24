a04ccf67f778d5ab314ec1252ae4c1c1f4404453
Cristiano Fromagio <cristianofromagio@gmail.com>
2022-05-24 00:10:29 -0300

    Update icon and setup extension page_action

--- background.js
+++ background.js
@@ -4,18 +4,6 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo) => {
 
 function init(tabId) {
 
-    var title = browser.i18n.getMessage("extensionTitle");
-
-    browser.pageAction.setIcon({
-        tabId: tabId,
-        path: "icons/icon.png"
-    });
-
-    browser.pageAction.setTitle({
-        tabId: tabId,
-        title: title
-    });
-    
     browser.pageAction.setPopup({
         tabId: tabId,
         popup: "popup-img/index.html"
